a2205dc76fee4084e670cf9cd08e8c579016777a
Alex Hortin <hortinstein@gmail.com>
2014-11-18 20:47:12 -0500

    setting up deleiverable 2

--- GradesCalculator/test/edu/gatech/seclass/gradescalc/GradesDBTest.java
+++ GradesCalculator/test/edu/gatech/seclass/gradescalc/GradesDBTest.java
@@ -1,8 +1,15 @@
 package edu.gatech.seclass.gradescalc;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.HashSet;
 
 import org.junit.After;
@@ -12,10 +19,17 @@ import org.junit.Test;
 public class GradesDBTest {
 
     GradesDB db = null;
-    static final String GRADES_DB = "DB/GradesDatabase6300.xlsx";
+    static final String GRADES_DB_GOLDEN = "DB" + File.separator
+            + "GradesDatabase6300-goldenversion.xlsx";
+    static final String GRADES_DB = "DB" + File.separator
+            + "GradesDatabase6300.xlsx";
 
     @Before
     public void setUp() throws Exception {
+        FileSystem fs = FileSystems.getDefault();
+        Path dbfilegolden = fs.getPath(GRADES_DB_GOLDEN);
+        Path dbfile = fs.getPath(GRADES_DB);
+        Files.copy(dbfilegolden, dbfile, REPLACE_EXISTING);
         db = new GradesDB(GRADES_DB);
     }
 
@@ -84,4 +98,70 @@ public class GradesDBTest {
         Student student = db.getStudentByID("901234504");
         assertTrue(student.getName().compareTo("Shevon Wise") == 0);
     }
+
+    // New tests
+    @Test
+    public void testAddAssignment() {
+        db.addAssignment("ASSIGNMENT: black-box testing");
+        db = new GradesDB(GRADES_DB);
+        assertEquals(4, db.getNumAssignments());
+        db.addAssignment("ASSIGNMENT: white-box testing");
+        db = new GradesDB(GRADES_DB);
+        assertEquals(5, db.getNumAssignments());
+    }
+
+    @Test
+    public void testAddGradesForAssignment() {
+        String assignmentName = "ASSIGNMENT: category partition";
+        Student student1 = new Student("Josepha Jube", "901234502", db);
+        Student student2 = new Student("Christine Schaeffer", "901234508", db);
+        Student student3 = new Student("Ernesta Anderson", "901234510", db);
+        db.addAssignment(assignmentName);
+        db = new GradesDB(GRADES_DB);
+        HashMap<Student, Integer> grades = new HashMap<Student, Integer>();
+        grades.put(student1, 87);
+        grades.put(student2, 94);
+        grades.put(student3, 100);
+        db.addGradesForAssignment(assignmentName, grades);
+        db = new GradesDB(GRADES_DB);
+        assertEquals(90, db.getAverageAssignmentsGrade(student1), 1);
+        assertEquals(94, db.getAverageAssignmentsGrade(student2), 1);
+        assertEquals(93, db.getAverageAssignmentsGrade(student3), 1);
+    }
+
+    @Test
+    public void testGetAverageAssignmentsGrade() {
+        // Rounded to the closest integer
+        Student student = new Student("Ernesta Anderson", "901234510", db);
+        assertEquals(90, db.getAverageAssignmentsGrade(student), 1);
+    }
+
+    @Test
+    public void testGetAverageProjectsGrade() {
+        // Rounded to the closest integer
+        Student student = new Student("Rastus Kight", "901234512", db);
+        assertEquals(86, db.getAverageProjectsGrade(student), 1);
+    }
+
+    @Test
+    public void testAddIndividualContributions() {
+        String projectName1 = "PROJECT 2";
+        Student student1 = new Student("Josepha Jube", "901234502", db);
+        Student student2 = new Student("Grier Nehling", "901234503", db);
+        HashMap<Student, Integer> contributions1 = new HashMap<Student, Integer>();
+        contributions1.put(student1, 96);
+        contributions1.put(student2, 87);
+        db.addIndividualContributions(projectName1, contributions1);
+        db = new GradesDB(GRADES_DB);
+        String projectName2 = "PROJECT 3";
+        HashMap<Student, Integer> contributions2 = new HashMap<Student, Integer>();
+        contributions2.put(student1, 98);
+        contributions2.put(student2, 100);
+        db.addIndividualContributions(projectName2, contributions2);
+        assertEquals(90, db.getAverageProjectsGrade(student1), 1);
+        assertEquals(84, db.getAverageProjectsGrade(student2), 1);
+    }
+    // //////////////////////////////
+    // NO CHANGES ABOVE THIS LINE //
+    // //////////////////////////////
 }
