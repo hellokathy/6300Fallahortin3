e3a81ec3dd7f2884980cd8be6723df654bfcff25
Alex Hortin <hortinstein@gmail.com>
2014-11-18 23:19:52 -0500

    solving deliverable2
    lots of new methods added

--- GradesCalculator/src/edu/gatech/seclass/gradescalc/GradesDB.java
+++ GradesCalculator/src/edu/gatech/seclass/gradescalc/GradesDB.java
@@ -1,10 +1,14 @@
 package edu.gatech.seclass.gradescalc;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.HashSet;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -14,10 +18,24 @@ public class GradesDB {
 	XSSFSheet sheetTeamGrades;
 	XSSFSheet sheetSoloGrades;
 	XSSFSheet sheetAttendance;
-
-	public GradesDB(String dbName) throws IOException{
-		InputStream inp = new FileInputStream(dbName);
-		XSSFWorkbook wb = new XSSFWorkbook(inp);
+	XSSFSheet sheetProjects;
+	XSSFSheet sheetContribs;
+	InputStream inp = null;
+	XSSFWorkbook wb = null;
+	FileOutputStream fileOut;
+	String dbName;
+	
+	
+	public GradesDB(String dbName)  {
+		this.dbName = dbName;
+		try {
+			inp = new FileInputStream(dbName);
+			wb = new XSSFWorkbook(inp);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 		this.sheetSoloGrades = wb.getSheet("IndividualGrades"); //Student Assignments
 		this.sheetTeamGrades = wb.getSheet("TeamGrades");
@@ -72,6 +90,61 @@ public class GradesDB {
 			}
 		}
 		return null;
+	}
+	
+	private void saveBook(){
+		FileOutputStream fileOut;
+		try {
+			fileOut = new FileOutputStream(this.dbName);
+			this.wb.write(fileOut);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void addAssignment(String string) {
+		Row header = this.sheetSoloGrades.getRow(0);
+		int num_assignments = header.getLastCellNum(); //subtracts name
+		Cell cell = header.createCell(num_assignments);
+		cell.setCellValue(string);
+		saveBook();
+	}
+
+	public void addGradesForAssignment(String assignmentName,
+			HashMap<Student, Integer> grades) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private Row seekStudent(XSSFSheet sheet,String name){
+		for (int rowNum = 1; rowNum < sheet.getLastRowNum()+1; rowNum++){
+			Row row = sheet.getRow(rowNum);
+			if (row.getCell(0).getStringCellValue().compareTo(name)==0){ //checking to see if the row equals the name
+				return row;
+			}
+		}
+		return null;
+	}
+	
+	public double getAverageAssignmentsGrade(Student student) {
+		Row row = this.seekStudent(this.sheetSoloGrades, student.getName());
+		int totalGrade = 0;
+		for (int colNum = 1; colNum <= this.getNumAssignments();colNum++){
+			totalGrade += row.getCell(colNum).getNumericCellValue();
+		}
+		return totalGrade/this.getNumAssignments();
+	}
+
+	public double getAverageProjectsGrade(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void addIndividualContributions(String projectName1,
+			HashMap<Student, Integer> contributions1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
+++ GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
@@ -5,12 +5,19 @@ public class Student {
 	int gtID;
 	//String email;
 	int attendance;
-	public Student(String name, int gtID, int attendance){
+	public Student(String name, String id, GradesDB db){ //added the new method for deliverable 2
 		this.name = name;
-		this.gtID = gtID;
-		this.attendance = Integer.valueOf(attendance);
+		Student s = db.getStudentByID(id);
+		//this.gtID = Integer.getInteger(s.getGtid());
+		this.attendance = s.attendance;
 	};
 	
+	public Student(String name, int id, int attendance) {
+		this.name = name;
+		this.gtID = id;
+		this.attendance = Integer.valueOf(attendance);
+	}
+
 	public String getName() {
 		return this.name;
 	}
